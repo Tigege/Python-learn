bd638a4ebf653cd76a8d66dd520bd1b8cdb8477e
QingTai Jiang <jiangqingtai@qq.com>
2018-04-19 21:37:27 +0800

    2018/4/19

---
+++ suiji/test.py
@@ -0,0 +1,25 @@
+from random import *
+s= open(r"C:\Users\Administrator\Desktop\submission.csv",'w',encoding='utf-8')
+
+s.close()
+num=0
+
+with open(r"C:\Users\Administrator\Desktop\test1.csv",'r') as f:
+    with open(r"C:\Users\Administrator\Desktop\submission.csv", 'a',encoding='utf-8') as s:
+        for i in f:
+            num=num+1
+
+            l = i.strip()
+            ll=l.split(',')
+          #  print(ll)
+
+            if num==1:
+                pass
+            else:
+                number=round(uniform(0,1),7)
+
+                s.write(str(ll[0])+','+str(ll[1]) + ',' + str(number) +'\n')
+print("over")
+
+
+
